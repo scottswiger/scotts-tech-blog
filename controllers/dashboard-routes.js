2731171116ed2fd4e52c77d27778f500d4a17cc8
sswiger <sswiger@forhims.com>
2023-05-16 21:14:35 -0600

    added some routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Post } = require('../models/');
+const withAuth = require('../utils/auth');
+
+//Get posts on home page
+router.get('/', withAuth, async (req, res) => {
+  try {
+    postData = await Post.findAll
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // fill in the view to be rendered
+    res.render('hmmmm what goes here', {
+      // this is how we specify a different layout other than main! no change needed
+      layout: 'dashboard',
+      // coming from line 10 above, no change needed
+      posts,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+router.get('/new', withAuth, (req, res) => {
+  // what view should we send the client when they want to create a new-post? (change this next line)
+  res.render('hmmmm what goes here', {
+    // again, rendering with a different layout than main! no change needed
+    layout: 'dashboard',
+  });
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    // what should we pass here? we need to get some data passed via the request body
+    const postData = await Post.findByPk(????);
+
+    if (postData) {
+      // serializing the data
+      const post = postData.get({ plain: true });
+      // which view should we render if we want to edit a post?
+      res.render('hmmmm what goes here', {
+        layout: 'dashboard',
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+module.exports = router;
